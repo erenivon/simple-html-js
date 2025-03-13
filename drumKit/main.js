510c2be31185fbe987abe4e2d504dc5a5a311769
Erenivon <erenyilmazwtc@gmail.com>
2025-03-13 13:54:26 +0300

    responsive fixes

--- drumKit/main.js
+++ drumKit/main.js
@@ -1,27 +1,37 @@
-let char = document.querySelectorAll(".char p");
+
+let char = document.querySelectorAll("p");
+function playMusic(i){
+  new Audio(`audio/${i}.wav`).play();
+  char[Number(i-1)].style.transform = "scale(0.8)";
+  char[Number(i-1)].style.color = "#ffc800";
+  setTimeout(() => {
+    char[Number(i-1)].style.transform = "scale(1)";
+    char[Number(i-1)].style.color = "#fff";
+  }, 300);
+}
 document.addEventListener("keydown", (e) => {
   if (e.code == "KeyA") {
-    new Audio("audio/usbcikar.wav").play();
+    new Audio("audio/1.wav").play();
     char[0].style.transform = "scale(0.8)";
     char[0].style.color = "#ffc800";
   }
   if (e.code == "KeyS") {
-    new Audio("audio/usbsok.wav").play();
+    new Audio("audio/2.wav").play();
     char[1].style.transform = "scale(0.8)";
     char[1].style.color = "#ffc800";
   }
   if (e.code == "KeyD") {
-    new Audio("audio/uyari.wav").play();
+    new Audio("audio/3.wav").play();
     char[2].style.transform = "scale(0.8)";
     char[2].style.color = "#ffc800";
   }
   if (e.code == "KeyF") {
-    new Audio("audio/uyari3.wav").play();
+    new Audio("audio/4.wav").play();
     char[3].style.transform = "scale(0.8)";
     char[3].style.color = "#ffc800";
   }
   if (e.code == "KeyG") {
-    new Audio("audio/uyari2.wav").play();
+    new Audio("audio/5.wav").play();
     char[4].style.transform = "scale(0.8)";
     char[4].style.color = "#ffc800";
   }
