bfea333ac2c9bebd99fe4aa0908306f503b503ee
Erenivon <erenyilmazwtc@gmail.com>
2025-03-17 15:08:31 +0300

    updates

--- animBar/main.js
+++ animBar/main.js
@@ -1,10 +1,16 @@
 const checkbox = document.getElementById("checkbox");
 const daynight = document.getElementById("daynight");
 
+let dayText = document.getElementById("day");
+
 checkbox.addEventListener("click", () => {
   if (checkbox.checked) {
     daynight.classList.add("checked");
+    document.body.style.backgroundColor = "#022152"
+    dayText.innerText = "NIGHT"
   } else {
     daynight.classList.remove("checked");
+    dayText.innerText = "DAY"
+    document.body.style.backgroundColor = "#5d77a3"
   }
 });
